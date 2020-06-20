932b5e7df064ad9fd8739a447d4c8309fd53f1e6
Jeff Martin <jeff@vrbo.com>
2020-06-20 12:48:37 +0100

    Implement CatsResource for scalatest

---
+++ scalatest/src/main/scala/cats/effect/testing/scalatest/CatsResourceIO.scala
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2020 Daniel Spiewak
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cats.effect.testing.scalatest
+
+import cats.effect._
+import org.scalatest.AsyncTestSuite
+
+trait CatsResourceIO[A] extends CatsResource[IO, A] with AsyncIOSpec {
+  asyncTestSuite: AsyncTestSuite =>
+
+  implicit def ResourceEffect: Effect[IO] = IO.ioEffect
+
+  def resource: Resource[IO, A]
+
+}
---
+++ scalatest/src/test/scala/cats/effect/testing/scalatest/CatsResourceSpecs.scala
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2020 Daniel Spiewak
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cats.effect.testing.scalatest
+
+import cats.effect._
+import cats.effect.concurrent.Ref
+import org.scalatest.matchers.must.Matchers._
+import org.scalatest.wordspec.AsyncWordSpec
+
+class CatsResourceSpecs extends AsyncWordSpec with CatsResourceIO[Ref[IO, Int]] {
+
+  override def resource: Resource[IO, Ref[IO, Int]] =
+    Resource.make(Ref[IO].of(0))(_.set(Int.MinValue))
+
+  "cats resource specifications" should {
+    "run a resource modification" in withResource { ref =>
+      ref
+        .modify { a =>
+          (a + 1, a)
+        }
+        .map(
+          _ must be(0)
+        )
+    }
+
+    "be shared between tests" in withResource { ref =>
+      ref
+        .modify { a =>
+          (a + 1, a)
+        }
+        .map(
+          _ must be(1)
+        )
+    }
+  }
+}
