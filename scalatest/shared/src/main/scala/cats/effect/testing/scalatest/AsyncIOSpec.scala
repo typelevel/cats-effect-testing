ff22817528546d06bbcc043f86d3bb1131dc6a92
Arman Bilge <armanbilge@gmail.com>
2022-06-03 03:13:41 +0000

    Don't use default EC for scalatest

--- scalatest/shared/src/main/scala/cats/effect/testing/scalatest/AsyncIOSpec.scala
+++ scalatest/shared/src/main/scala/cats/effect/testing/scalatest/AsyncIOSpec.scala
@@ -27,7 +27,9 @@ import org.scalatest.time.Span
 
 trait AsyncIOSpec extends AssertingSyntax with EffectTestSupport with RuntimePlatform { asyncTestSuite: AsyncTestSuite =>
 
-  implicit lazy val ioRuntime: IORuntime = createIORuntime(executionContext)
+  override implicit def executionContext = ioRuntime.compute
+
+  implicit lazy val ioRuntime: IORuntime = IORuntime.global
 
   implicit def ioRetrying[T]: Retrying[IO[T]] = new Retrying[IO[T]] {
     override def retry(timeout: Span, interval: Span, pos: Position)(fun: => IO[T]): IO[T] =
